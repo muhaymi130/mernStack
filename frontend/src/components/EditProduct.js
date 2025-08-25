a2e3fd3e9ba65cd9ca8a3d36a3ab336a77e54af8
muhaymi130 <muhayminurdin130@gmail.com>
2025-08-25 11:08:34 +0700

    fix: add frontend as normal folder instead of submodule

---
+++ frontend/src/components/EditProduct.js
@@ -0,0 +1,102 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useParams, useNavigate } from "react-router-dom";
+
+const EditProduct = () => {
+  const [title, setTitle] = useState("");
+  const [file, setFile] = useState("");
+  const [preview, setPreview] = useState("");
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    getProductsById();
+  }, []);
+
+  const getProductsById = async () => {
+    const response = await axios.get(`http://localhost:5000/products/${id}`);
+    setTitle(response.data.name);
+    setFile(response.data.image);
+    setPreview(response.data.url);
+  };
+
+  const loadImage = (e) => {
+    const image = e.target.files[0];
+    setFile(image);
+    setPreview(URL.createObjectURL(image));
+  };
+
+  const updateProduct = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("file", file);
+    formData.append("name", title);
+
+    try {
+      await axios.patch(`http://localhost:5000/products/${id}`, formData, {
+        headers: {
+          "Content-type": "multipart/form-data",
+        },
+      });
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div className="column is-centered mt-5">
+      <div className="column is-half">
+        <form onSubmit={updateProduct}>
+          <div className="field">
+            <label className="label">image</label>
+            <div className="control">
+              <input
+                type="text"
+                className="input"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                placeholder="product name"
+              ></input>
+            </div>
+          </div>
+          <div className="field">
+            <label className="label">image</label>
+            <div className="control">
+              <div className="file">
+                <label className="file-label">
+                  <input
+                    type="file"
+                    className="file-input"
+                    onChange={loadImage}
+                  />
+                  <span className="file-cta">
+                    <span className="file-label">choose a file..</span>
+                  </span>
+                </label>
+              </div>
+            </div>
+          </div>
+
+          {preview ? (
+            <figure className="image is-128x128">
+              <img src={preview} alt="preview Image" />
+            </figure>
+          ) : (
+            ""
+          )}
+
+          <div className="field">
+            <div className="control">
+              <button type="submit" className="button is-success">
+                Update
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default EditProduct;
